50f953466894c345852ec4cafdd9bc64b47c33e0
Alvaro Bezerra <alvaro.a.bezerra@hmit.com.br>
2024-12-18 09:04:28 -0300

    Primeiro form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,13 @@
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 export function Home() {
+
+  function handleParticipantAdd() {
+    console.log('Você clicou no botão de adicionar');
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -12,11 +17,19 @@ export function Home() {
       <Text style={styles.eventDate}>
         Sexta, 4 de Novembro de 2022
       </Text>
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
 
-      <TextInput 
-        style={styles.input}
-        
-      />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -17,10 +17,31 @@ export const styles = StyleSheet.create({
     fontSize: 16,
   },
   input: {
+    flex: 1,
     height: 56,
     backgroundColor: '#1F1E25',
-    borderRadius: 10, color: '#FFFFFF',
+    borderRadius: 5,
+    color: '#FFFFFF',
     padding: 16,
-    fontSize: 16
+    fontSize: 16,
+    marginRight: 12
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 24
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#32Cf67',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42
   }
 });
